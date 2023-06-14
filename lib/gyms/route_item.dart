f4c8f8e5cc340e60a89f03b7d832bf463e147cef
azon <32076614+AZ0N@users.noreply.github.com>
2023-06-14 10:38:11 +0200

    add cached_network_image package and change route images to CachedNetworkImage

--- lib/gyms/route_item.dart
+++ lib/gyms/route_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'gym_overview.dart';
@@ -22,12 +23,14 @@ class RouteItem extends StatelessWidget {
             if (climbingRoute.imageUrl.isNotEmpty)
               ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  climbingRoute.imageUrl,
+                child: CachedNetworkImage(
+                  imageUrl: climbingRoute.imageUrl,
                   height: 200,
                   width: double.infinity,
                   fit: BoxFit.cover,
                   alignment: Alignment.topCenter,
+                  placeholder: (context, url) =>
+                      const Center(child: CircularProgressIndicator()),
                 ),
               ),
             const SizedBox(height: 5),

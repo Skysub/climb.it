c27617301a38797aadd65db17ca6cdbfe8fd9929
oscarsvenstrup <129071972+oscarsvenstrup@users.noreply.github.com>
2023-06-14 14:21:51 +0200

    Updated profile + Chart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,16 +1,27 @@
 
+import 'package:climb_it/bar%20chart/bar_graph.dart';
 import 'package:climb_it/main_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   ProfilePage({super.key});
 
+  final List<double> vAmounts = [
+    0.0,
+    1.0,
+    2.0,
+    3.0,
+    4.0,
+    5.0,
+    6.0,
+    7.0,
+  ];
   final List<String> centers = ['Center1', 'Center2', 'Center3'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MainAppBar(barTitle: 'Profile'),
+      appBar: MainAppBar(barTitle: 'Profile'),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Padding(
@@ -52,32 +63,15 @@ class ProfilePage extends StatelessWidget {
                   ],
                 ),
               ),
-              const SizedBox(height: 50),
-              const Align(
-                  alignment: Alignment.centerLeft, child: Text('Routes:')),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('0'),
-                    Text('1'),
-                    Text('2'),
-                    Text('3'),
-                    Text('4'),
-                    Text('5'),
-                    Text('6'),
-                    Text('7'),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 5),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  
-                ],
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  child: SizedBox(
+                    height: 200,
+                    child: MyBarGraph(vAmmount: vAmounts,
+                    ),
+                  ),
+                )
               ),
               const SizedBox(height: 40),
               Row(
@@ -105,3 +99,15 @@ class ProfilePage extends StatelessWidget {
     );
   }
 }
+
+
+/*
+       
+      body: Center(
+        child: SizedBox(
+          height: 200,
+          child: MyBarGraph(
+            vAmmount: vAmounts,
+          )),
+      ),
+      */

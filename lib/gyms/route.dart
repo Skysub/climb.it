9b2ec4a9bf1cc50876a2138a2e9d24fb9c507016
CBSaksager <Christiansaksager@gmail.com>
2023-06-14 12:41:04 +0200

    Update route.dart now has videoplayer

--- lib/gyms/route.dart
+++ lib/gyms/route.dart
@@ -1,16 +1,71 @@
 import 'package:climb_it/gyms/gym_overview.dart';
 import 'package:climb_it/main_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class RoutePage extends StatelessWidget {
+class RoutePage extends StatefulWidget {
   const RoutePage({super.key, required this.route});
 
   final ClimbingRoute route;
 
   @override
+  State<RoutePage> createState() => _RoutePageState();
+}
+
+class _RoutePageState extends State<RoutePage> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4");
+    //_controller = VideoPlayerController.asset("videos/sample_video.mp4");
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: MainAppBar(barTitle: route.name),
-        body: Center(child: Text(route.name)));
+      appBar: MainAppBar(barTitle: widget.route.name),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          Text(widget.route.name),
+          Image.asset(
+              'boulderingrouteholder.jpg'), //TODO get picture from firebase
+          FutureBuilder(
+            future: _initializeVideoPlayerFuture,
+            builder: (context, snapshot) {
+              if(snapshot.connectionState == ConnectionState.done) {
+                return Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller)),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            } 
+          ),
+          FloatingActionButton(
+            onPressed: () {
+                        setState(() {
+                          if (_controller.value.isPlaying) {
+                            _controller.pause();
+                          } else {
+                            _controller.play();
+                          }
+                        });
+                      },
+          )
+        ],
+      )),
+    );
   }
 }
